e0b7f9402c7e3289d5a633f74e00dc39e074cff2
MOV-EDX <61068227+MOV-EDX@users.noreply.github.com>
2023-12-04 20:30:44 +0000

    Refactored to meet C# coding standards and updated to .NET 8

--- Hex Decoder/Form1.cs
+++ Hex Decoder/Form1.cs
@@ -12,31 +12,31 @@ namespace Hex_Decoder
 
         private MatchCollection GetMatches()
         {
-            Regex pattern = new Regex("((?<=hex:)|(?<=hex\\(\\d\\):))[a-z0-9\\,]*");
-            Regex white_space = new Regex("\\s+");
+            var pattern = new Regex("((?<=hex:)|(?<=hex\\(\\d\\):))[a-z0-9\\,]*");
+            var whiteSpace = new Regex("\\s+");
 
-            string source = Source_TextBox.Text.Replace("\\", string.Empty).Replace("\n", string.Empty);
-            source = white_space.Replace(source, string.Empty);
+            var source = Source_TextBox.Text.Replace("\\", string.Empty).Replace("\n", string.Empty);
+            source = whiteSpace.Replace(source, string.Empty);
 
             return pattern.Matches(source);
         }
 
         private void ASCIIConvert_Button_Click(object sender, EventArgs e)
         {
-            StringBuilder builder = new StringBuilder();
+            var builder = new StringBuilder();
 
             //Match multiple values
-            MatchCollection matches = GetMatches();
+            var matches = GetMatches();
 
             foreach (Match match in matches)
             {
-                string hex_string = match.Value.Replace(",", string.Empty);
-                byte[] bytes = Convert.FromHexString(hex_string);
-                string result = Encoding.ASCII.GetString(bytes);
+                var hexString = match.Value.Replace(",", string.Empty);
+                byte[] bytes = Convert.FromHexString(hexString);
+                var result = Encoding.ASCII.GetString(bytes);
 
                 if (result.Contains("\0"))
                 {
-                    Regex pattern = new Regex("^b(\\0)*");
+                    var pattern = new Regex("^b(\\0)*");
                     result = pattern.Replace(result, string.Empty);
                 }
 
@@ -48,21 +48,21 @@ namespace Hex_Decoder
 
         private void UTF16Convert_Button_Click(object sender, EventArgs e)
         {
-            StringBuilder builder = new StringBuilder();
+            var builder = new StringBuilder();
 
             //Match multiple values
-            MatchCollection matches = GetMatches();
+            var matches = GetMatches();
 
             foreach (Match match in matches)
             {
-                string hex_string = match.Value.Replace(",", string.Empty);
-                byte[] bytes = Convert.FromHexString(hex_string);
-                string result = Encoding.Unicode.GetString(bytes);
+                var hexString = match.Value.Replace(",", string.Empty);
+                byte[] bytes = Convert.FromHexString(hexString);
+                var result = Encoding.Unicode.GetString(bytes);
 
                 //Remove null-terminated characters
                 if (result.EndsWith("\0"))
                 {
-                    Regex pattern = new Regex("\\0*");
+                    var pattern = new Regex("\\0*");
                     result = pattern.Replace(result, string.Empty);
                 }
 
@@ -74,16 +74,16 @@ namespace Hex_Decoder
 
         private void Latin1_Convert_Button_Click(object sender, EventArgs e)
         {
-            StringBuilder builder = new StringBuilder();
+            var builder = new StringBuilder();
 
             //Match multiple values
-            MatchCollection matches = GetMatches();
+            var matches = GetMatches();
 
             foreach (Match match in matches)
             {
-                string hex_string = match.Value.Replace(",", string.Empty);
-                byte[] bytes = Convert.FromHexString(hex_string);
-                string result = Encoding.Latin1.GetString(bytes);
+                var hexString = match.Value.Replace(",", string.Empty);
+                byte[] bytes = Convert.FromHexString(hexString);
+                var result = Encoding.Latin1.GetString(bytes);
 
                 builder.AppendLine(result);
             }
@@ -93,18 +93,18 @@ namespace Hex_Decoder
 
         private void Convert_S256H_Button_Click(object sender, EventArgs e)
         {
-            StringBuilder builder = new StringBuilder();
+            var builder = new StringBuilder();
 
             //Match multiple values
-            MatchCollection matches = GetMatches();
+            var matches = GetMatches();
 
             foreach (Match match in matches)
             {
-                string hex_string = match.Value.Replace(",", string.Empty);
-                byte[] bytes = Convert.FromHexString(hex_string);
-                string result = Convert.ToBase64String(bytes);
+                var hexString = match.Value.Replace(",", string.Empty);
+                byte[] bytes = Convert.FromHexString(hexString);
+                var result = Convert.ToBase64String(bytes);
 
-                builder.AppendLine(hex_string.ToUpper());
+                builder.AppendLine(hexString.ToUpper());
                 builder.AppendLine(result);
             }
 
